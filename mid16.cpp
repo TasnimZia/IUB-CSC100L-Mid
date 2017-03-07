09744c5599b6fd0b86bbaaee844c9cf6876103b0
TasnimZia <tasnimzia97@gmail.com>
2017-03-07 15:49:41 +0600

    Update mid16.cpp

--- mid16.cpp
+++ mid16.cpp
@@ -21,21 +21,21 @@ int main()      //This is the main function
 
     n=(b^2)-4*a*c;      //Formula to solve the operation
 
-    if (n==0)
+    if (n==0)       //Checks whether the answer is equal to 0
     {
         cout<<"equal roots"<<endl;      //Displays the nature of roots to the user
     }
 
-    else if (n>=0)
+    else if (n>=0)      //Checks whether the answer is greater than or equal to 0
     {
         cout<<"Real roots"<<endl;       //Displays the nature of roots to the user
     }
 
-    else if (n<0)
+    else if (n<0)             //Checks whether the answer is less than 0
     {
         cout<<"Imaginary roots"<<endl;      //Displays the nature of roots to the user
     }
-    else
+    else        //Checks whether the answer is greater than 0
     {
         cout<<"Unequal roots"<<endl;       //Displays the nature of roots to the user
     }
