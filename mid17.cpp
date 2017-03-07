2fbe3b787a023dc30f26dbc96c13e81746eb2862
TasnimZia <tasnimzia97@gmail.com>
2017-03-07 15:53:19 +0600

    Update mid17.cpp

--- mid17.cpp
+++ mid17.cpp
@@ -11,35 +11,33 @@ int main()      //This is the main function
 
     cin>>c;      //Takes user input and stores it in variable
 
-    if (c<0)
+    if (c<0)        //Checks whether the temperature is less than 0
     {
         cout<<"Freezing weather"<<endl;     //Displays the message to the user
     }
 
-    else if (c<=10)
+    else if (c<=10)     //Checks whether the temperature is less than or equal to 10
     {
         cout<<"Very cold weather"<<endl;        //Displays the message to the user
     }
 
-
-    else if (c<=20)
+    else if (c<=20)     //Checks whether the temperature is less than or equal to 20
     {
         cout<<"Cold weather"<<endl;     //Displays the message to the user
     }
 
-
-    else if (c<=30)
+    else if (c<=30)     //Checks whether the temperature is less than or equal to 30
     {
         cout<<"Normal"<<endl;       //Displays the message to the user
     }
 
-    else if (c<=40)
+    else if (c<=40)             //Checks whether the temperature is less than or equal to 40
     {
         cout<<"It's hot"<<endl;     //Displays the message to the user
     }
 
-    else
-    {
+    else        //Checks whether the temperature is greater than 40
+    {       
         cout<<"Very hot"<<endl;     //Displays the message to the user
     }
 
