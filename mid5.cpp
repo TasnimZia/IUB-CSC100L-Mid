167bb6d6422982c4bdac15517574c3ecc21c7c22
TasnimZia <tasnimzia97@gmail.com>
2017-03-07 17:12:56 +0600

    Update mid5.cpp

--- mid5.cpp
+++ mid5.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()      //This is the main function
 {   //Start of main body
 
-    string a;       //Declares string variable with label : a
+    string x;      //Declares string variable with label : x
 
-    cout<<"Please enter the value of a:"<<endl;     //Prompts the user for the value of the string
+    cout<<"Please enter a string: "<<endl;      //Prompts the user for number
 
-    cin>>a;     //Takes user input and stores it in variable
+    cin>>x;     //Takes user input and stores it in variable
 
-    cout<<"Your value of double is:"<<a<<endl;     //Displays the value to the user
+   cout<<endl<<"outside integer"<<endl;
 
-    return 0;    //End of program
-}   //End of main body
+   double b=double (x);
+
+   cout<<b<<endl;    //Displays the number to the user
+
+return 0;       //End of program
+} //End of main body
