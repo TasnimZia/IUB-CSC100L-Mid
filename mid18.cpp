6652cc2d0c9b4766ce90813a17bcf56d8a34a97f
TasnimZia <tasnimzia97@gmail.com>
2017-03-08 12:08:48 +0600

    18

---
+++ mid18.cpp
@@ -0,0 +1,70 @@
+#include <iostream>     //This includes InputOutput Library
+
+using namespace std;
+
+int main()      //This is the main function
+{ //Start of main body
+
+    char x;     //Declares character variable with label : x
+
+    cout<<"Enter a letter: "<<endl;      //Prompts the user for a letter
+
+    cin>>x;     //Takes user input and stores it in variable
+
+    if (x=='a')     //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+    else if (x=='A')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+    else if (x=='e')        //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+    else if (x=='E')        //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+     else if (x=='i')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+     else if (x=='I')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+     else if (x=='o')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+     else if (x=='O')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+     else if (x=='u')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+     else if (x=='U')       //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a vowel"<<endl;        //Displays the result to the user
+    }
+
+    else        //Checks whether the letter is a vowel or a consonant
+    {
+        cout<<"It is a consonant"<<endl;        //Displays the result to the user
+    }
+
+ return 0;       //End of program
+} //End of main body
